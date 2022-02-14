7c6f507fb239a8c549c1f85f8d273183dddd6c7d
Anjali-18 <60153249+Anjali-18@users.noreply.github.com>
2022-02-14 15:37:46 +0530

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+    
+} from 'react-admin';
+
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" allowEmpty>
+        <SelectInput optionText="name" />
+    </ReferenceInput>,
+];
+export const PostList = props => (
+    <List {...props}>
+       <Datagrid rowClick="edit">
+       <Datagrid>
+           <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+           <TextField source="id" />
+            <TextField source="title" />
+           <TextField source="body" />
+           <EditButton />
+        </Datagrid>
+        </Datagrid>
+    </List>
+);
+        
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = props => (
+        
+        <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+           <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+               <SelectInput optionText="id" />
+               <SelectInput optionText="name" />
+           </ReferenceInput>
+           <TextInput source="id" />
+            <TextInput source="title" />
+           <TextInput source="body" />
+           <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+    
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
